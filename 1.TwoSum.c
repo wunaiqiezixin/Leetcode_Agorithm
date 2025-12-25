633affe97ddee2b0caced25204191e20e787893b
wunaiqiezixin <1947685764@qq.com>
2025-12-25 22:57:56 +0800

    Refactor twoSum for improved clarity and efficiency
    Refactor twoSum function to simplify memory allocation and return logic.

--- 1.TwoSum.c
+++ 1.TwoSum.c
@@ -2,26 +2,22 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    for (int i = 0;i < numsSize - 1;i++)
+    *returnSize = 0;
+    int *result;
+    int i,j;
+    for (i = 0;i < numsSize - 1;i++)
     {
-        for (int j = i+1;j < numsSize;j++)
-        {   
+        for (j = i + 1;j < numsSize;j++)
+        {
             if (nums[i] + nums[j] == target)
             {
-               int* result = (int*)malloc(sizeof(int)*2);
-               if (result == NULL)
-               {
-                *returnSize = 0;
-                return NULL;
-               }
-               result[0] = i;
-               result[1] = j;
-               *returnSize = 2;
-               return result;
-
+                *returnSize = 2;
+                result = (int*)malloc(sizeof(int)*2);
+                result[0] = i;
+                result[1] = j;
+                return result;
             }
         }
     }
-    *returnSize = 0;
     return NULL;
 }
